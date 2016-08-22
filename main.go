64bbea31a41b9774d1d606141bf68f3d9306d288
Rabinzon <ildarrazin@gmail.com>
2016-08-22 08:22:15 +0300

    refactor(main): add print message

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"./src/queue/"
+	"fmt"
 )
 
 func main() {
-	q := queue.NewQueue()
-	q.Add(1);
-	q.Add(2);
-	q.Add(3);
-	q.Pop()
-	q.PrintAll();
+	fmt.Println("packages in src >")
 }
